3e1804988aa9536f77da334e1f4b59dcdf41376b
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-11-02 12:45:20 +0100

    02_11_24

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,17 +6,17 @@ export const navLinks = [
   },
   {
     id: 2,
-    name: 'About',
+    name: 'Über uns',
     href: '#about',
   },
   {
     id: 3,
-    name: 'Work',
+    name: 'Projekte',
     href: '#work',
   },
   {
     id: 4,
-    name: 'Contact',
+    name: 'Kontakt',
     href: '#contact',
   },
 ];
@@ -263,7 +263,8 @@ export const workExperiences = [
     name: 'Framer',
     pos: 'Lead Web Developer',
     duration: '2022 - Present',
-    title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
+    title:
+      'Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.',
     icon: '/assets/framer.svg',
     animation: 'victory',
   },
@@ -272,7 +273,8 @@ export const workExperiences = [
     name: 'Figma',
     pos: 'Web Developer',
     duration: '2020 - 2022',
-    title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
+    title:
+      'Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.',
     icon: '/assets/figma.svg',
     animation: 'clapping',
   },
@@ -281,7 +283,8 @@ export const workExperiences = [
     name: 'Notion',
     pos: 'Junior Web Developer',
     duration: '2019 - 2020',
-    title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
+    title:
+      'Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.',
     icon: '/assets/notion.svg',
     animation: 'salute',
   },
