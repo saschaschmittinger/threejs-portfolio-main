e8ed8f872c4d64bc22bf5dca5a0c9c8f36f5430c
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-11-03 21:32:53 +0100

    03_11_24

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,17 @@ import HackerRoom from '../components/HackerRoom';
 import { Suspense } from 'react';
 import CanvasLoader from '../components/CanvasLoader';
 
+import { useMediaQuery } from 'react-responsive';
+import { calculateSizes } from '../constants/index';
+import Target from '../components/Target';
+
 const Hero = () => {
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -17,7 +27,16 @@ const Hero = () => {
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
             <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-            <HackerRoom scale={0.07} position={[0, 0, 0]} rotation={[0, 180, 0]} />
+            <HackerRoom // scale={0.07}
+              position={[1.7, -10, 1.9]}
+              rotation={[0.2, -Math.PI, 0]}
+              scale={isMobile ? 0.07 : 0.1}
+            />
+
+            <group>
+              <Target position={sizes.targetPosition} />
+            </group>
+
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
           </Suspense>
