ae61f61e14782b5d5bbc7933fe3d80a6d1571bf3
Sascha Schmittinger <saschatimoschmittinger@gmail.com>
2024-11-04 17:55:47 +0100

    04_11_24

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,4 @@
+import { Leva } from 'leva';
 import { PerspectiveCamera } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import HackerRoom from '../components/HackerRoom';
@@ -7,8 +8,14 @@ import CanvasLoader from '../components/CanvasLoader';
 import { useMediaQuery } from 'react-responsive';
 import { calculateSizes } from '../constants/index';
 import Target from '../components/Target';
+import ReactLogo from '../components/ReactLogo';
+import Cube from '../components/Cube';
+import Rings from '../components/Rings';
+import HeroCamera from '../components/HeroCamera.jsx';
+import Button from '../components/Button.jsx';
 
 const Hero = () => {
+  // Use media queries to determine screen size
   const isSmall = useMediaQuery({ maxWidth: 440 });
   const isMobile = useMediaQuery({ maxWidth: 768 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
@@ -16,31 +23,42 @@ const Hero = () => {
   const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
   return (
-    <section className="min-h-screen w-full flex flex-col relative">
+    <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
-          Hallo ich bin Sascha <span className="waving-hand">👋</span>
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+          Hi, Ich bin Sascha <span className="waving-hand">👋</span>
         </p>
         <p className="hero_tag text-gray_gradient">Building Websites & Brands</p>
       </div>
+
       <div className="w-full h-full absolute inset-0">
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
+            {/* To hide controller */}
+            <Leva hidden />
             <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-            <HackerRoom // scale={0.07}
-              position={[1.7, -10, 1.9]}
-              rotation={[0.2, -Math.PI, 0]}
-              scale={isMobile ? 0.07 : 0.1}
-            />
+
+            <HeroCamera isMobile={isMobile}>
+              <HackerRoom scale={sizes.deskScale} position={sizes.deskPosition} rotation={[0.1, -Math.PI, 0]} />
+            </HeroCamera>
 
             <group>
               <Target position={sizes.targetPosition} />
+              <ReactLogo position={sizes.reactLogoPosition} />
+              <Rings position={sizes.ringPosition} />
+              <Cube position={sizes.cubePosition} />
             </group>
 
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
           </Suspense>
         </Canvas>
+      </div>
+
+      <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+        <a href="#about" className="w-fit">
+          <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
+        </a>
       </div>
     </section>
   );
